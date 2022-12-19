df5fbdb419378bf80b29aaaf53a33a25309045f9
pattanaponjuajun1 <112550576+pattanaponjuajun1@users.noreply.github.com>
2022-12-19 16:10:20 +0700

    Node V2

--- app.js
+++ app.js
@@ -1,8 +1,14 @@
 const express = require('express');
 const chalk = require('chalk');
 const debug = require('debug')('app');
+const morgan = require('morgan');
+const path = require('path');
+
 const app = require('express')();
 const port = 5000;
+
+app.use(morgan('combined'));
+app.use(express.static(path.join(__dirname, "/public")))
 
 app.get("/", (req,res) => {
 
