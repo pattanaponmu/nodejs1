384bd61cca4e18d29fca931ee50946405d44e834
pattanaponjuajun1 <112550576+pattanaponjuajun1@users.noreply.github.com>
2022-12-20 15:39:08 +0700

    last not clound

--- app.js
+++ app.js
@@ -3,16 +3,25 @@ const chalk = require('chalk');
 const debug = require('debug')('app');
 const morgan = require('morgan');
 const path = require('path');
+const productRouter = express.Router();
 
 const app = require('express')();
-const port = 5000;
+const port = process.env.PORT; //http://localhost:5000/
 
 app.use(morgan('combined'));
-app.use(express.static(path.join(__dirname, "/public")))
+app.use(express.static(path.join(__dirname, "/public")));
+
+app.set("views", "./src/views");
+app.set("view engine", "ejs");
+
+/*productRouter.route("/1").get((req.res) => {
+    res.send("I am Product 1");
+});
+app.use("/products", productRouter)*/
 
 app.get("/", (req,res) => {
 
-        res.send('Hello PongNode'); 
+        res.render('index', {username: "Ponggg", customer: ["AA", "BB","CC","DDD","EEEEE"]}); 
 
     }
 )
